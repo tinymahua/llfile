e375f8502a645842872dc55b6e61ca1f3ed7a29e
Tiny.Twist <tiny.mahua@gmail.com>
2024-12-20 15:25:49 +0800

    Add l10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:llfile/src/rust/api/simple.dart';
 import 'package:llfile/src/rust/frb_generated.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<void> main() async {
   await RustLib.init();
@@ -13,15 +14,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: const Locale('en'),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: Scaffold(
         appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-              'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`'),
-        ),
+        body: L10nTestWidget(),
       ),
     );
   }
 }
+
+
+class L10nTestWidget extends StatelessWidget {
+  const L10nTestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(AppLocalizations.of(context)!.hello("张三")));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
